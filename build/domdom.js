6aa937832fed53d9da502e1d0622fd57a0f9dd96
Stefanos Vichas <stefanos@vichas.org>
2019-02-14 22:40:32 +0200

    minor updates

--- build/domdom.js
+++ build/domdom.js
@@ -1,7 +1,67 @@
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var domdom = function domdom() {
-     _classCallCheck(this, domdom);
-};
+/**
+* domdom
+* Project: https://github.com/svichas/domdom
+* License: MIT
+* Author: Stefanos Vichas
+* Version: 0.1.0
+*/
+
+var domdom = function () {
+     function domdom() {
+          _classCallCheck(this, domdom);
+     }
+
+     _createClass(domdom, [{
+          key: "create",
+          value: function create(elementType) {
+               var element = document.createElement(elementType);
+               return new domdomElement(element);
+          }
+     }, {
+          key: "select",
+          value: function select(selector) {
+               var element = document.querySelector(selector);
+
+               if (element) {
+                    return new domdomElement(element);
+               }
+
+               return false;
+          }
+     }]);
+
+     return domdom;
+}();
+
+var domdomElement = function () {
+     function domdomElement(element) {
+          _classCallCheck(this, domdomElement);
+
+          this.DOMDOMELEMENT = true;
+          this.element = element;
+          return this;
+     }
+
+     _createClass(domdomElement, [{
+          key: "addClass",
+          value: function addClass(classname) {
+               this.element.classList.add(classname);
+               return this;
+          }
+     }, {
+          key: "appendTo",
+          value: function appendTo(element) {
+               if (!element.DOMDOMELEMENT) return this;
+               element.element.appendChild(this.element);
+               return this;
+          }
+     }]);
+
+     return domdomElement;
+}();
--- src/domdom.js
+++ src/domdom.js
@@ -10,11 +10,46 @@ class domdom {
 
      constructor() {
 
+
+
      }
 
-     create() {
+     create(elementType) {
+          let element = document.createElement(elementType);
+          return new domdomElement(element);
+     }
+
+     select(selector) {
+          let element = document.querySelector(selector);
+
+          if (element) {
+               return new domdomElement(element);
+          }
+
+          return false;
+     }
+
+}
 
 
+
+class domdomElement {
+
+     constructor(element) {
+          this.DOMDOMELEMENT = true;
+          this.element = element;
+          return this;
+     }
+
+     addClass(classname) {
+          this.element.classList.add(classname);
+          return this;
+     }
+
+     appendTo(element) {
+          if (!element.DOMDOMELEMENT) return this;
+          element.element.appendChild(this.element);
+          return this;
      }
 
 }
